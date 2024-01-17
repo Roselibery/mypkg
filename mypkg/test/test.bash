a63a4056295daea3b7db8fee9fba5726c1f04dca
Roselibery <147678850+Roselibery@users.noreply.github.com>
2024-01-18 02:00:00 +0900

    Update test.bash

--- mypkg/test/test.bash
+++ mypkg/test/test.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2024 Yuri Sawada 　　　　　
+# SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
-[ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
+[ "$1" != "" ] && dir="$1"   
 
 cd $dir/ros2_ws
 colcon build
